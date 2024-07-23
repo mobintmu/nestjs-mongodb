6bbf2074203405efe8e88dd66b7ca92dcf47f03c
mobin.shaterian <mobinest@gmail.com>
2024-07-23 14:49:41 +0330

    [Add] GraphQL

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # nestjs-mongodb
+
+## Running the app
+
+```bash
+# development
+$ yarn run start
+
+# watch mode
+$ yarn run start:dev
+
+# production mode
+$ yarn run start:prod
+```
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,14 @@
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: 'schema.gql',
+    }),
+  ],
   controllers: [],
   providers: [],
 })
